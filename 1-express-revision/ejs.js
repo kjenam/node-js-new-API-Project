c842c9910b39167bd0c7d7450a27b5706ff035bf
kjenam <115461165+kjenam@users.noreply.github.com>
2026-01-11 22:17:00 +0530

    Update ejs.js
    Mind your language

--- 1-express-revision/ejs.js
+++ 1-express-revision/ejs.js
@@ -1,4 +1,5 @@
 // ejs is a templating engine which allows us to write html inside of javascript
 
-// react is just better fuck this thingy
+// react is just better than javaScript in some aspects
+
 
